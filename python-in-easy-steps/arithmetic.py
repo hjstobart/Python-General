8efd82d538081b28af90c93139a5aff2fd02b666
Harry <98965340+hjstobart@users.noreply.github.com>
2022-06-28 09:06:54 +0100

    Add files via upload

---
+++ python-in-easy-steps/arithmetic.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun  6 11:08:13 2021
+
+@author: harry
+"""
+
+a = 8
+b = 2
+
+print('Addition: \t \t' , a , '+' , b , '=' , a+b )
+print('Subtraction: \t' , a , '-' , b , '=' , a-b )
+print('Multiplication: \t' , a , '*' , b , '=' , a*b )
+print('Division: \t \t' , a , '/' , b , '=' , a/b )
+print('Floor division: \t' , a , '//' , b , '=' , a//b )
+print('Modulo: \t \t \t' , a , '%' , b , '=' , a%b )
+print('Exponent: \t \t' , a , '^2 = ' , a**b , sep='')
+
+
+
+
