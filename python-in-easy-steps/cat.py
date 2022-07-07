42248dc29d47ba551da566c3e641aa5ebd8c4630
Harry <98965340+hjstobart@users.noreply.github.com>
2022-07-07 08:57:08 +0100

    Add files via upload

---
+++ python-in-easy-steps/cat.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 11 20:56:16 2021
+
+@author: harry
+"""
+
+def purr( pet = 'A Cat') :
+    print( pet , 'Says MEOW!')
+    
+def lick( pet = 'A Cat') :
+    print( pet , 'Drinks milk.')
+
+def nap( pet = 'A Cat') :
+    print( pet , 'Sleep by the fire')
+    
