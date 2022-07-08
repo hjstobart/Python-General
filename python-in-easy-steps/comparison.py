10c22634e877d5e497265167b7e11a76efc33124
Harry <98965340+hjstobart@users.noreply.github.com>
2022-07-08 09:18:00 +0100

    Add files via upload

---
+++ python-in-easy-steps/comparison.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun  6 14:51:58 2021
+
+@author: harry
+"""
+
+nil = 0
+num = 0
+max = 1
+cap = 'A'
+low = 'a'
+
+print('Equality:\t' , nil , '==' , num , nil == num)
+print('Equality:\t' , cap , '==' , low , cap == low)
+print('Inequality:\t' , nil , '!=' , max , nil != max)
+print('Greater:\t\t' , nil , '>' , max , nil > max)
+print('Lesser:\t\t' , nil , '<' , max , nil < max)
+print('More or equal:\t' , nil , '>=' , num , nil >= num)
+print('Less or equal:\t' , max , '<=' , num , max <= num)
