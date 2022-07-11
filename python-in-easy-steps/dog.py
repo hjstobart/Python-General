ce0e225fbddeff1c96730e57a28d13580c42764a
Harry <98965340+hjstobart@users.noreply.github.com>
2022-07-11 09:04:06 +0100

    Add files via upload

---
+++ python-in-easy-steps/dog.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 11 21:25:40 2021
+
+@author: harry
+"""
+
+def bark(pet = 'A Dog') :
+    print(pet , 'Says Woof!')
+
+def lick(pet = 'A Dog') :
+    print(pet , 'Drinks Water')
+
+def nap(pet = 'A Dog') :
+    print(pet , 'Sleeps in the Sun')
+    
