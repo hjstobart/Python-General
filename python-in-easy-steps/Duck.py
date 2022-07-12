6155927fcfa8205584c1457f19783f4becdad677
Harry <98965340+hjstobart@users.noreply.github.com>
2022-07-12 09:05:53 +0100

    Add files via upload

---
+++ python-in-easy-steps/Duck.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 16 17:53:59 2021
+
+@author: harry
+"""
+
+class Duck :
+    def talk(self):
+        print('\nDuck says: Quack!')
+        
+    def coat(self):
+        print('\nDuck wears: Feathers')
