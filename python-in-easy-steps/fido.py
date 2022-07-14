6dbfd01cf914cfad81eea1896a24bafd9ea5ee74
Harry <98965340+hjstobart@users.noreply.github.com>
2022-07-14 09:06:57 +0100

    Add files via upload

---
+++ python-in-easy-steps/fido.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 11 21:29:32 2021
+
+@author: harry
+"""
+
+from dog import *
+
+pet = input('Enter a pet name: ')
+
+bark(pet)
+lick(pet)
+nap(pet)
