1b6d2d3632e33c84d953ad4f0fe9bd41c3bc6eb7
Harry <98965340+hjstobart@users.noreply.github.com>
2022-07-18 08:19:46 +0100

    Add files via upload

---
+++ python-in-easy-steps/garbage.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 16 17:27:11 2021
+
+@author: harry
+"""
+
+from Songbird import *
+
+bird_1 = Songbird('Koko' , 'Tweet, tweet!\n')
+print(bird_1.name , 'ID:' , id(bird_1))
+bird_1.sing()
+
+del bird_1
+
+bird_2 = Songbird('Louie' , 'Chirp, chirp!\n')
+print(bird_2.name , 'ID:' , id(bird_2))
+bird_2.sing()
+
+bird_3 = Songbird('Misty' , 'Squawk, squawk!\n')
+print(bird_3.name , 'ID:' , id(bird_3))
+bird_3.sing()
+
+del bird_2
+del bird_3
