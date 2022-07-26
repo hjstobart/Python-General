46c0dbcaecb1a8c2cd2aa58d1443c0e80c3727ca
Harry <98965340+hjstobart@users.noreply.github.com>
2022-07-26 17:55:46 +0100

    Add files via upload

---
+++ python-in-easy-steps/input.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun  6 10:48:16 2021
+
+@author: harry
+"""
+
+# Initialize a variable with a user-specified value.
+user = input('I am Python. What is your name? : ')
+
+# Output a string and a variable value.
+print('Welcome' , user)
+
+# Initialize another variable with a user-specified value.
+lang = input('Favourite programming language? : ')
+
+# Output a string and a variable value.
+print( lang, 'Is', 'Fun', sep = " * " , end = '!\n')
+
