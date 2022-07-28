40e4bc7933eb1e1c197866677ca75ac21c0643e8
Harry <98965340+hjstobart@users.noreply.github.com>
2022-07-28 11:38:43 +0100

    Add files via upload

---
+++ python-in-easy-steps/kitty.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 11 21:22:11 2021
+
+@author: harry
+"""
+
+import cat 
+
+cat.purr()
+cat.lick()
+cat.nap()
+
+cat.purr('Kitty')
+cat.lick('Kitty')
+cat.nap('Kitty')
+
