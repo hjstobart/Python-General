b9ad1fde1b55415f685f26a33cf1028c6e776408
Harry <98965340+hjstobart@users.noreply.github.com>
2022-07-30 09:40:11 +0100

    Add files via upload

---
+++ python-in-easy-steps/list.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun  6 15:51:57 2021
+
+@author: harry
+"""
+
+quarter = [ 'January' , 'February' , 'March' ]
+
+print('First month: ' , quarter[0])
+print('Second month: ' , quarter[1])
+print('Third month: ' , quarter[2])
+
+coords = [ [1, 2, 3] , [4, 5, 6]]
+
+print('\n Top left 0,0 : ' , coords[0][0])
+print('Bottom right 1,2 : ' , coords[1][2])
+
+print('\n Second month first letter: ' , quarter[1][0])
