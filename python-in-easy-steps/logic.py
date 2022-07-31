e49be4fcaffb4c4cb61275bcd3bb2aebf9ebd672
Harry <98965340+hjstobart@users.noreply.github.com>
2022-07-31 10:18:40 +0100

    Add files via upload

---
+++ python-in-easy-steps/logic.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun  6 15:19:06 2021
+
+@author: harry
+"""
+
+a = True
+b = False
+
+print('\n AND logic: ')
+print('a and a' , a and a)
+print('a and b' , a and b)
+print('b and b' , b and b)
+
+print('\n OR logic: ')
+print('a or a' , a or a)
+print('a or b' , a or b)
+print('b or b' , b or b)
+
+print('\n NOT logic: ')
+print('a =' , a , '\t not a =' , not a)
+print('b = ' , b , '\t not b =' , not b)
+
