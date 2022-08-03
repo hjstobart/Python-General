bcb4f6b2716426abe10bad3d759e59fb38928133
Harry <98965340+hjstobart@users.noreply.github.com>
2022-08-03 10:01:34 +0100

    Add files via upload

---
+++ python-in-easy-steps/manipulate.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 15 14:52:44 2021
+
+@author: harry
+"""
+
+def display(s) :
+    '''Display an argument value'''
+    print(s)
+    
+display(display.__doc__)
+
+display(r'C:\Program Files')
+
+display('\nHello' + ' Python')
+
+display('Python in easy steps\n' [7:])
+
+display('P' in 'Python')
+
+display('p' in 'Python')
