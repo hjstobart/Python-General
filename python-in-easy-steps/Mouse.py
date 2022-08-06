ffd055cda8900a5cf9e2bedef18fb8623047f412
Harry <98965340+hjstobart@users.noreply.github.com>
2022-08-06 10:42:22 +0100

    Add files via upload

---
+++ python-in-easy-steps/Mouse.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 16 17:54:54 2021
+
+@author: harry
+"""
+
+class Mouse :
+    def talk(self) :
+        print('\nMouse says: Squeak!')
+    
+    def coat(self) :
+        print('\nMouse wears: Fur')
