a7cc6dcc2c799d96e61428038dc531e94db7f182
Harry <98965340+hjstobart@users.noreply.github.com>
2022-08-09 09:18:15 +0100

    Add files via upload

---
+++ python-in-easy-steps/pass.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun  8 14:55:26 2021
+
+@author: harry
+"""
+
+bool = True
+if bool :
+    print('Python in easy steps')
+else :
+    pass
