d51cbe6f2bafbf9799030ff92e3ec96710f27e0a
Harry <98965340+hjstobart@users.noreply.github.com>
2022-08-16 09:45:22 +0100

    Add files via upload

---
+++ python-in-easy-steps/Polygon.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 16 17:33:15 2021
+
+@author: harry
+"""
+
+class Polygon :
+    width = 0
+    height = 0 
+    def set_values(self, width , height) :
+        Polygon.width = width
+        Polygon.height = height
+        
+        
