02190b3fa58d7e1766e206b5659830e8b519bf1d
Harry <98965340+hjstobart@users.noreply.github.com>
2022-08-18 08:41:03 +0100

    Add files via upload

---
+++ python-in-easy-steps/pooch.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 11 21:28:34 2021
+
+@author: harry
+"""
+
+from dog import bark , lick , nap
+
+bark()
+lick()
+nap()
+
+bark('Pooch')
+lick('Pooch')
+nap('Pooch')
