4d1c4fdead9839bf319628e283fc82f1d5b19951
Harry <98965340+hjstobart@users.noreply.github.com>
2022-08-24 16:09:33 +0100

    Add files via upload

---
+++ python-in-easy-steps/runtime.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun  6 11:00:00 2021
+
+@author: harry
+"""
+
+# The error is NameError: name 'titel' is not defined
+
+title = 'Python in easy steps'
+print(titel)
