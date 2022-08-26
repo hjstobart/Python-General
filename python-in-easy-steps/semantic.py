22b5a727a257a5177d083d64299bd51e17330e42
Harry <98965340+hjstobart@users.noreply.github.com>
2022-08-26 11:13:07 +0100

    Add files via upload

---
+++ python-in-easy-steps/semantic.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun  6 11:01:20 2021
+
+@author: harry
+"""
+
+num = 3
+print( num * 8 + 4 )
+
+# Incorrect answer comes out becuase there are no brackets.
+
+num = 3
+print( num * (8+4))
