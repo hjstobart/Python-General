47ea1aa47f96331472413e2200fd5e8fc2c47ffe
Harry <98965340+hjstobart@users.noreply.github.com>
2022-08-30 10:08:35 +0100

    Add files via upload

---
+++ python-in-easy-steps/syntax.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun  6 10:57:24 2021
+
+@author: harry
+"""
+# Note the missing quotation mark in the print function.
+# The error will be returned as SyntaxError: EOL while scanning string literal
+print('Python in easy steps)
