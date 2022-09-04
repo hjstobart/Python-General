8562d22c77316e4d9d17e270d7ec3a1703169fba
Harry <98965340+hjstobart@users.noreply.github.com>
2022-09-04 18:31:33 +0100

    Add files via upload

---
+++ python-in-easy-steps/tiger.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 11 21:23:23 2021
+
+@author: harry
+"""
+
+import cat
+
+pet = input('Enter a pet name: ')
+
+cat.purr(pet)
+cat.lick(pet)
+cat.nap(pet)
